041530e68457ee091d426cea57a9cfac0571709b
arkamaldeen <kabdurrahmaan28@gmail.com>
2024-01-06 21:25:18 +0530

    Update Home.jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Settings from './pages/Settings';
+import { AppStateProvider } from './AppStateContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStateProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<Home />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AppStateProvider>
   );
 }
 
